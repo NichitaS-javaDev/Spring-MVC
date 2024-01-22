0b07f92755eb9f69bbc7bc901cb22ddd2812067d
nichita <nichita.saharov@iongroup.com>
2024-01-22 17:36:14 +0200

    incident details window added

--- src/main/java/org/example/controller/HelpDeskIncidentController.java
+++ src/main/java/org/example/controller/HelpDeskIncidentController.java
@@ -4,10 +4,14 @@ import org.example.model.HelpDeskIncident;
 import org.example.repo.IHelpDeskIncidentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @Controller
 public class HelpDeskIncidentController {
@@ -26,5 +30,17 @@ public class HelpDeskIncidentController {
     @GetMapping("/")
     public String incidents() {
         return "incidents";
+    }
+
+    @GetMapping("/details/{id}")
+    public String details(@PathVariable("id") UUID id, Model model){
+        try {
+            HelpDeskIncident incident = incidentRepo.findById(id).orElseThrow();
+            model.addAttribute("incident", incident);
+        } catch (NoSuchElementException e){
+            model.addAttribute("error", e.getMessage());
+        }
+
+        return "details";
     }
 }
