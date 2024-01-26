5135c18a8cb898b173cf4ee883ec736e3d302a9f
nichita <nichita.saharov@iongroup.com>
2024-01-26 12:32:38 +0200

    delete unused

--- src/main/java/org/example/repo/IHelpDeskIncidentOriginRepo.java
+++
@@ -1,9 +0,0 @@
-package org.example.repo;
-
-import org.example.model.HelpDeskIncidentOrigin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface IHelpDeskIncidentOriginRepo extends JpaRepository<HelpDeskIncidentOrigin, UUID> {
-}
--- src/main/java/org/example/repo/IHelpDeskIncidentScopeRepo.java
+++
@@ -1,9 +0,0 @@
-package org.example.repo;
-
-import org.example.model.HelpDeskIncidentScope;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface IHelpDeskIncidentScopeRepo extends JpaRepository<HelpDeskIncidentScope, UUID> {
-}
